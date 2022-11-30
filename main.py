0e6a3c105aa8f97ad13eeb17a89a93c9f8065480
kdy91202 <kdy91202@gmail.com>
2022-11-30 18:51:57 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def precision(cluster, Gtcluster) :
+    for i in range(len(cluster)) :
+        return
+
+
+def recall(cluster, Gtcluster) :
+    return
+
+def fscore(precision, recall) :
+    sums = precision + recall
+    multis = precision * recall
+    calc1 = multis / sums
+    result = calc1 * 2
+    return result
